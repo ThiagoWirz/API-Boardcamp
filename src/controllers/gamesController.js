24188a44bd502f0f7aafe0644e3c05550e881b0e
Thiago <thiagowirzdemoura@gmail.com>
2022-03-03 06:23:37 -0300

    feat: get games with query

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -8,6 +8,11 @@ export async function getGames(req, res) {
       const { rows: games } = await db.query("SELECT * FROM games");
       return res.send(games);
     }
+    const { rows: games } = await db.query(
+      "SELECT * FROM games WHERE LOWER (name) LIKE LOWER($1)",
+      [name]
+    );
+    res.send(games);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
