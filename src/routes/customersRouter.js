ec4bfbb4f07ccf02a05f86675ab2dcd1646e12f5
Thiago <thiagowirzdemoura@gmail.com>
2022-03-07 04:09:37 -0300

    feat: validatecustomerUpdate

--- src/routes/customersRouter.js
+++ src/routes/customersRouter.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import {
+  createCustomer,
   getCustomer,
   getCustomers,
 } from "../controllers/customersController.js";
-import { validateCustomer } from "../middlewares/validateCustomerMiddleware.js";
+import {
+  validateCustomer,
+  validateCustomerUpdate,
+} from "../middlewares/validateCustomerMiddleware.js";
 
 const customersRouter = Router();
 
 customersRouter.get("/customers", getCustomers);
 customersRouter.get("/customers/:id", getCustomer);
 customersRouter.post("/customers", validateCustomer, createCustomer);
+customersRouter.put("/customers/:id", validateCustomerUpdate);
