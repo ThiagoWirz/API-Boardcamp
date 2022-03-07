c7280c0b61e7fa562850cde4ad6ba8721127c886
Thiago <thiagowirzdemoura@gmail.com>
2022-03-06 22:47:17 -0300

    feat: get customers with cpf in query

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -7,6 +7,12 @@ export async function getCustomers(req, res) {
       const { rows: customers } = await db.query("SELECT * FROM customers");
       return res.send(customers);
     }
+
+    const { rows: customers } = await db.query(
+      "SELECT * FROM customers WHERE cpf LIKE ($1)",
+      [`${cpf}%`]
+    );
+    return res.send(customers);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
